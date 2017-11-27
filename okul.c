86333de4d5f36cb7d1ef3a4dc36e8eef1be4b6dd
Taha Yasir Kıroğlu <yasir.kiroglu@bil.omu.edu.tr>
2017-11-27 22:22:37 +0300

    main güncellendi

--- okul.c
+++ okul.c
@@ -7,24 +7,24 @@
 int main (int argc, char *argv[]) {
 	FILE * file;
 	int line_count, control;
-	char file_name[] = "okul.csv", argument;
+	char file_name[] = "okul.csv", arg_char;
 
-	argument_control (argc, argv[1]);
-	
-	/* arguman verildi mi diye bakar */
-	argument = (argc > 1)? argv[1][0]: 0;
+	control = argument_control (argc, argv[1]);
 
-	/* dosyayı okumaya çalış ulaşmazsan hata ver */
+	/* dosyayı okumaya çalış, ulaşmazsan hata ver */
 	file  = read_file (file_name);
 
 	/* dosyadaki eleman sayısı */
 	line_count = fgetlinecount (file);
 
+  /* dosyada delimiter kontrolü (3 tane ',' ara) yap */
 	control = fdeliControl (file, ',', 3, line_count);
-	
-	if (control)
-		csv_error ();
 
+  /* delimiter okumada problem varsa hata ver ve çık */
+	if (control)
+		stu_error (CSV_ERROR_MESSAGE);
+
+  /* öğrenci dizisini oluştur */
 	STU stus[line_count];
 
 	/* elemanları verilen dosyadan çek*/
@@ -35,11 +35,13 @@ int main (int argc, char *argv[]) {
 	line_count -= control;
 	
 	/* elemanları sıralat */
-	qsort (stus, line_count, sizeof(STU), compare_for_stu);	
-
+	qsort (stus, line_count, sizeof(STU), compare_for_stu);
+	
+	/* puts stu için arg_char parametresini hazırlar */
+	arg_char = (argc > 1)? argv[1][0]: 0;
 
 	/* verilen elemanları eleman sayısınca ekrana bas */
-	puts_stus (stus, line_count, argument);
+	puts_stus (stus, line_count, arg_char);
 
 	/* bellekten alınan yeri geri ver */
 	free_stus (stus, line_count);
